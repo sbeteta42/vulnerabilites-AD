c83f17b301648f250927cba1e6df400b12e8815d
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2025-11-19 14:47:38 +0100

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 echo "[+] Mise à jour système"
-sudo apt update && sudo apt full-upgrade -y
+sudo apt update
 
 echo "[+] Installation des outils du TP"
 sudo apt install -y metasploit-framework responder hashcat crackmapexec enum4linux-ng nmap smbclient impacket-scripts bloodhound.py python3-pip
-
-echo "[+] Configuration Metasploit"
-sudo systemctl start postgresql && sudo msfdb init
-
-echo "[+] Installation Responder (dernière version)"
-cd /opt
-sudo git clone https://github.com/lgandx/Responder.git
-cd Responder
-sudo pip3 install -r requirements.txt
 
 echo "[+] Tout est prêt !"
 echo "Test Responder : sudo python3 /opt/Responder/Responder.py -I eth0 -dw"
